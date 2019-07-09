429ce3c81bc20c35c08a6113857b6076885c155d
mforoni <marcofor10@gmail.com>
2019-07-10 00:01:02 +0100

    improve test on stdout for kernel print

--- rspec-demo/std1/question_spec.rb
+++ rspec-demo/std1/question_spec.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
-def first_question
-  puts 'Do you know how to play? (yes or no)'
+QUESTION = 'Do you know how to play? (yes or no)'
+
+def first_question(question = QUESTION)
+  puts question
   answer = STDIN.gets.chomp.downcase
   answer
+end
+
+def top_print_me(msg)
+  print msg
+end
+
+def kernel_print_me(msg)
+  Kernel.print msg
+end
+
+class Foo
+  def self.print_me(msg)
+    print msg
+  end
 end
 
 context '#first_question' do
@@ -19,11 +35,11 @@ context '#first_question' do
     first_question
 
     expect(STDOUT).to have_received(:puts).with(
-      'Do you know how to play? (yes or no)'
+      QUESTION
     ).once
 
     expect(Kernel).to_not have_received(:puts).with(
-      'Do you know how to play? (yes or no)'
+      QUESTION
     )
   end
 
@@ -35,5 +51,48 @@ context '#first_question' do
 
   it 'returns the sanitized input' do
     expect(first_question).to eq 'yes'
+  end
+end
+
+context '#top_print_me' do
+  before do
+    allow(Kernel).to receive(:print)
+    allow(STDOUT).to receive(:print)
+  end
+
+  it 'print the msg but not with Kernel.print' do
+    msg = 'This is the message'
+    top_print_me(msg)
+
+    expect(STDOUT).to_not have_received(:print)
+    expect(Kernel).to_not have_received(:print)
+  end
+end
+
+context '#kernel_print_me' do
+  before do
+    allow(Kernel).to receive(:print)
+    allow(STDOUT).to receive(:print)
+  end
+
+  it 'print the msg' do
+    msg = 'This is the message'
+    kernel_print_me(msg)
+
+    expect(STDOUT).to_not have_received(:print)
+    expect(Kernel).to have_received(:print).once
+  end
+end
+
+describe Foo do
+  context '#print_me' do
+    it 'print the msg' do
+      allow(Foo).to receive(:print)
+
+      msg = 'This is the message'
+      Foo.print_me(msg)
+
+      expect(Foo).to have_received(:print).once
+    end
   end
 end
